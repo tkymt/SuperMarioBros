19ca75d218c7bec40a1be2919c6e27c2c2e7dd5d
Taku Yamamoto <jts0ymt@outlook.jp>
2024-01-25 17:46:30 +0900

    Pic.hを書いた

---
+++ Pic.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "DxLib.h"
+
+class {
+public:
+	int Tit;
+	int Bac;
+	int Bl1;
+	int Bl2;
+	int Bl3;
+	int Bl4;
+	int Ene;
+	int Mar;
+
+	void Read() {
+		Tit = LoadGraph(_T("Images/Title.bmp"));
+		Bac = LoadGraph(_T("Images/Sky.bmp"));
+		Bl1 = LoadGraph(_T("Brick.bmp"));
+		Bl2 = LoadGraph(_T("Images/Empty_block.bmp"));
+		Bl3 = LoadGraph(_T("Images/Floor.bmp"));
+		Bl4 = LoadGraph(_T("Images/Question_Block.bmp"));
+		Ene = LoadGraph(_T("Enemy_Goomba.bmp"));
+		Mar = LoadGraph(_T("Images/Title.bpm"));
+	}
+}Pic;
+
