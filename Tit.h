de7a0fe131fa27fe6c14ef14aaa0d04ebab8d433
Taku Yamamoto <jts0ymt@outlook.jp>
2024-01-29 16:17:42 +0900

    タイトルからアクションに遷移するコードを書いた

--- Tit.h
+++ Tit.h
@@ -1,12 +1,18 @@
 #pragma once
 
 #include "DxLib.h"
+#include "tchar.h"
 #include "Pic.h"
 
 class {
 public:
 	void Out() {
 		DrawGraph(0, 0, Pic.Tit, TRUE);
+		DrawFormatStringToHandle(150, 300, Col.Bla, Fon.c[30], _T("Press Enter Key."));
+		if (Key.Key[KEY_INPUT_NUMPADENTER] == 1) {
+			// Music Play
+			Sce = MEN::MEN_01_Action;
+		}
 	}
 }Tit;
 
